3e80e00d0bfd01ff8e57b54d81213a80b13637b1
Kamil Mielnik <kamil.mielnik.km@gmail.com>
2018-03-20 11:48:45 +0100

    New string extension (Empty)

---
+++ ZaklepTo.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZaklepTo.Core.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool Empty(this string value)
+            => string.IsNullOrWhiteSpace(value);
+    }
+}
