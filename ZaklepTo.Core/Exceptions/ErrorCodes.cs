391b597cae8a6086099259d4357d04958abfc9b3
MaciejKaca <maciek.kaca@gmail.com>
2018-03-20 15:49:38 +0100

    Added Login Error Code to ErrorCodes.

--- ZaklepTo.Core/Exceptions/ErrorCodes.cs
+++ ZaklepTo.Core/Exceptions/ErrorCodes.cs
@@ -6,6 +6,7 @@ namespace ZaklepTo.Core.Exceptions
 {
     public static class ErrorCodes
     {
+        public static string InvalidLogin => "invalid_login";
         public static string InvalidFirstName => "invalid_firstname";
         public static string InvalidLastName => "invalid_lastname";
         public static string InvalidEmail => "invalid_email";
