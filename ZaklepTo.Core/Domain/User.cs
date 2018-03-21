12678506fc0a71fb0db4c66719767a089d891330
MaciejKaca <maciek.kaca@gmail.com>
2018-03-21 19:50:03 +0100

    + SetLogin validation.

--- ZaklepTo.Core/Domain/User.cs
+++ ZaklepTo.Core/Domain/User.cs
@@ -17,7 +17,6 @@ namespace ZaklepTo.Core.Domain
         public string Password { get; protected set; }
         public string Salt { get; protected set; }
         public DateTime CreatedAt { get; protected set; }
-        private Regex LoginPattern = new Regex("[A-Za-z0-9]{5,20}");
 
         //niektóre biblioteki mogą wymagać konstruktora bezparametrowego
         protected User()
@@ -36,11 +35,16 @@ namespace ZaklepTo.Core.Domain
             Password = password;
             Salt = salt;
             CreatedAt = DateTime.UtcNow;
+        }
 
-            if(string.IsNullOrWhiteSpace(login) || LoginPattern.IsMatch(login))
+        public void SetLogin(string login)
+        {
+            Regex LoginPattern = new Regex("[A-Za-z0-9]{5,20}");
+            if (string.IsNullOrWhiteSpace(login) || LoginPattern.IsMatch(login))
             {
-                throw new DomainException(ErrorCodes.InvalidLogin, "Login can't contain special characters, white spaces and be less than 5 and longer than 20 characters.");
+               throw new DomainException(ErrorCodes.InvalidLogin, "Login can't contain special characters, white spaces and be less than 5 and longer than 20 characters.");
             }
         }
     }
 }
+
