e40cbd04dccd8d8d978b5a10a476f2ee3cd0cc44
Bartosz Wajsowicz <b.wajsowicz@gmail.com>
2018-03-22 22:51:58 +0100

    Included AutoMapper

---
+++ ZaklepTo.Infrastucture/Mappers/AutoMapperConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+
+namespace ZaklepTo.Infrastucture.Mappers
+{
+    public static class AutoMapperConfig
+    {
+        // not implemented yet 
+    }
+}
