4c49cb29f4fd0f947b5c2f2cdde5bd0787746ace
Bartosz Wajsowicz <b.wajsowicz@gmail.com>
2018-03-23 23:33:39 +0100

    Fixed misspell in RestaurantDTO

--- ZaklepTo.Infrastucture/DTO/RestaurantDTO.cs
+++ ZaklepTo.Infrastucture/DTO/RestaurantDTO.cs
@@ -9,7 +9,7 @@ namespace ZaklepTo.Infrastucture.DTO
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public string Cousine { get; set; }
+        public string Cuisine { get; set; }
         public string Localization { get; set; }
         public IEnumerable<TableDTO> Tables { get; set; }
     }
