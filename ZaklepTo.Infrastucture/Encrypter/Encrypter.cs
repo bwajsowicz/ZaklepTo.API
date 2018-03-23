29d424a3849b001d540eb27fcb666d1fd08fb8f5
Bartosz Wajsowicz <b.wajsowicz@gmail.com>
2018-03-23 23:02:38 +0100

    +Encrypter

---
+++ ZaklepTo.Infrastucture/Encrypter/Encrypter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using ZaklepTo.Core.Extensions;
+using ZaklepTo.Infrastucture.Encrypter;
+
+namespace ZaklepTo.Infrastructure
+{
+    public class Encrypter : IEncrypter
+    {
+        private static readonly int DeriveBytesIterationsCount = 10000;
+        private static readonly int SaltSize = 40;
+
+        public string GetSalt(string password)
+        {
+            if (password.Empty())
+                throw new ArgumentException("Can't generate salt from empty value.", nameof(password));
+
+            var saltBytes = new byte[SaltSize];
+            var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(saltBytes);
+
+            return Convert.ToBase64String(saltBytes);
+        }
+
+        public string GetHash(string password, string salt)
+        {
+            if (password.Empty())
+                throw new ArgumentException("Can't be empty.", nameof(password));
+            if (salt.Empty())
+                throw new ArgumentException("Can't be empty.", nameof(salt));
+
+            var pbkdf2 = new Rfc2898DeriveBytes(password, GetBytes(salt), DeriveBytesIterationsCount);
+            return Convert.ToBase64String(pbkdf2.GetBytes(50));
+        }
+
+        private static byte[] GetBytes(string salt)
+        {
+            var bytes = new byte[salt.Length * sizeof(char)];
+            Buffer.BlockCopy(salt.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
+        }
+    }
+}
---
+++ ZaklepTo.Infrastucture/Encrypter/IEncrypter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZaklepTo.Infrastucture.Encrypter
+{
+    public interface IEncrypter
+    {
+        string GetSalt(string password);
+        string GetHash(string password, string salt);
+    }
+}
