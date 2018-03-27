c648b089b519cbfb9b10f1f9f55f3a751a666c06
Kamil Mielnik <kamil.mielnik.km@gmail.com>
2018-03-27 15:55:26 +0200

    +drafts of reservation and restaurant services

---
+++ ZaklepTo.Infrastucture/Services/Interfaces/IReservationService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ZaklepTo.Infrastucture.DTO;
+
+namespace ZaklepTo.Infrastucture.Services.Implementations
+{
+    public interface IReservationService
+    {
+        Task<IEnumerable<ReservationDTO>> GetAllAsync();
+        Task<IEnumerable<ReservationDTO>> GetAllActiveAsync();
+        Task<ReservationDTO> GetAsync(Guid id);
+        Task UpdateAsync(ReservationDTO reservationDto);
+        Task DeleteAsync(Guid id);
+        //TODO finish interface
+    }
+}
---
+++ ZaklepTo.Infrastucture/Services/Interfaces/IRestaurantService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ZaklepTo.Infrastucture.DTO;
+
+namespace ZaklepTo.Infrastucture.Services.Implementations
+{
+    public interface IRestaurantService
+    {
+        Task<IEnumerable<RestaurantDTO>> GetAllAsync();
+        Task<RestaurantDTO> GetAsync(Guid id);
+        Task UpdateAsync(RestaurantDTO restaurantDto);
+    }
+}
