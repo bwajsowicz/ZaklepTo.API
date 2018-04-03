565aadad8473eb95152b3a02303954cb8a3b8a1a
Kamil Mielnik <kamil.mielnik.km@gmail.com>
2018-04-03 21:22:19 +0200

    +Employe/Owner Service basic interfaces

---
+++ ZaklepTo.Infrastucture/Services/Interfaces/IOwnerService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ZaklepTo.Infrastructure.DTO;
+using ZaklepTo.Infrastructure.DTO.OnCreate;
+using ZaklepTo.Infrastructure.DTO.OnUpdate;
+
+namespace ZaklepTo.Infrastructure.Services.Interfaces
+{
+    public interface IOwnerService
+    {
+        Task<OwnerDTO> GetAsync(string login);
+        Task<IEnumerable<OwnerDTO>> GetAllAsync();
+        Task LoginAsync(string login, string password);
+        Task RegisterAsync(OwnerOnCreateDTO ownerDto);
+        Task UpdateAsync(OwnerOnUpdateDTO ownerDto);
+        Task ChangePassword(PasswordChange passwordChange);
+        Task DeleteAsync(string login);
+    }
+}
