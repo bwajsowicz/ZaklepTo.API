0fc2080b85c54358851d8145580b27b52006287d
Bartosz Wajsowicz <b.wajsowicz@gmail.com>
2018-05-19 17:15:33 +0200

    Data Initializer is well organized now.

--- ZaklepTo.Infrastucture/Services/Implementations/DataInitializer.cs
+++ ZaklepTo.Infrastucture/Services/Implementations/DataInitializer.cs
@@ -34,10 +34,6 @@ namespace ZaklepTo.Infrastructure.Services.Implementations
 
         public async Task SeedAsync()
         {
-           var customers = await _customerService.GetAllAsync();
-           if(customers.Any())
-           return;
-
             var random = new Random();
 
             var exampleFirstName = new List<string>
@@ -45,171 +41,252 @@ namespace ZaklepTo.Infrastructure.Services.Implementations
                 "Adam",
                 "Kamil",
                 "Bartosz",
-                "Jorge",
+                "Oskar",
                 "Jacek",
                 "Karol",
-                "Krzystof"
+                "Krzystof",
+                "Antoni",
+                "Jakub",
+                "Jan",
+                "Szymon",
+                "Franciszek",
+                "Filip",
+                "Aleksander",
+                "Mikołaj",
+                "Wojciech",
+                "Kacper",
+                "Michał",
+                "Marcel",
+                "Stanisław",
+                "Wiktor",
+                "Piotr",
+                "Igor",
+                "Leon",
+                "Mateusz",
+                "Maksymilian",
+                "Miłosz",
+                "Oliwier",
+                "Tomasz",
+                "Karol"
             };
 
             var exampleLastName = new List<string>
             {
                 "Kowalski",
                 "Nowak",
-                "Wojtyła",
-                "Lopez",
-                "Duda",
-                "Wałęsa",
-                "Jotaro"
+                "Lewandowski",
+                "Jankowski",
+                "Dąbrowski",
+                "Kaczmarek",
+                "Stępień",
+                "Wiśniewski",
+                "Kowalczyk",
+                "Wójcik",
+                "Kamiński",
+                "Zieliński",
+                "Mielnik",
+                "Mazur",
+                "Kwiatkowski",
+                "Krawczyk",
+                "Chyliński",
+                "Dąbrowski",
+                "Zając",
+                "Michalski",
+                "Pawłowski",
+                "Wróbel",
+                "Jabłoński",
+                "Wieczorek",
+                "Nowakowski",
+                "Olszewski",
+                "Adamczyk",
+                "Pawlak",
+                "Sikora",
+                "Śpiewak",
             };
 
-            for (var i = 1; i < 10; i++)
+            // Customer
+            var customers = await _customerService.GetAllAsync();
+            if (!customers.Any())
             {
-                var firstName = exampleFirstName[random.Next(exampleFirstName.Count)];
-                var lastName = exampleLastName[random.Next(exampleLastName.Count)];
-                var login = $"customer{i}";
-                var email = $"{login}@gmail.com";
-                var phone = $"{i}02345678";
-
-                var customer = new CustomerOnCreateDto()
+                for (var i = 0; i < 100; i++)
                 {
-                    Login = login,
-                    FirstName = firstName,
-                    Email = email,
-                    Phone = phone,
-                    LastName = lastName,
-                    Password = "!QAZxsw2"
-                };
+                    var firstName = exampleFirstName[random.Next(exampleFirstName.Count)];
+                    var lastName = exampleLastName[random.Next(exampleLastName.Count)];
+                    var login = $"customer{i}";
+                    var email = $"{login}@gmail.com";
+                    var phone = $"{i}02345678";
 
-                await _customerService.RegisterAsync(customer);
-            } //CustomerEntity
+                    var customer = new CustomerOnCreateDto()
+                    {
+                        Login = login,
+                        FirstName = firstName,
+                        Email = email,
+                        Phone = phone,
+                        LastName = lastName,
+                        Password = "qwerty1"
+                    };
+
+                    await _customerService.RegisterAsync(customer);
+                }
+            }
 
             var exampleRestaurantName = new List<string>
             {
                 "Ceglana",
-                "Spizarnia",
-                "Bachus",
-                "Ole_Bistro",
+                "Spiżarnia",
+                "Hospoda",
+                "Ole Bistro",
                 "Brazileriro",
                 "Kawusia",
-                "Bezka",
-                "Kicia",
-                "Merszmyl",
-                "Mordaler"
+                "Zamkowa",
+                "Pod Kotem",
+                "Rewers",
+                "Taberna del Puerto"
             };
 
             var exampleCousine = new List<string>
             {
-                "Chińska",
-                "Hiszpanska",
+                "Hiszpańska",
                 "Polska",
-                "Włoska"
+                "Włoska",
+                "Chińska"
             };
 
-            for (var i = 0; i < exampleRestaurantName.Count; i++)
+            // Tables
+            var tables = new List<Table>();
+            for (var j = 0; j < 100; j++)
             {
-                var tables = new List<Table>(); 
-                for (var j = 0; j < random.Next(5, 12); j++)
+                var table = new Table(random.Next(1, 5), (0, 0));
+                tables.Add(table);
+            }
+
+            // Restaurants
+            var restaurants = (await _restaurantService.GetAllAsync()).ToList();
+            if (!restaurants.Any())
+            {
+                for (var i = 0; i < 10; i++)
                 {
-                    var exampleTable = new Table(random.Next(1, 10), (random.Next(1, 100), random.Next(1, 100)));
+                    var restaurantTables = new List<Table>();
+                    for (var j = 0; j < 10; j++)
+                    {
+                        restaurantTables.Add(tables.ElementAt(j + (i * 10)));
+                    }
 
-                    tables.Add(exampleTable);
-                } // Tables
+                    var restaurant = new RestaurantOnCreateDto()
+                    {
+                        Name = exampleRestaurantName[i],
+                        Description =
+                            $@"Restauracja {
+                                    exampleRestaurantName[i]
+                                } jest od lat cenionym miejscem kameralnych spotkań, znanym ze znakomitej kuchni. Zapraszamy na  wyśmienity lunch w przerwie w pracy, wykwintny obiad z rodziną lub romantyczny wieczór na tarasach przy lampce wina.  Jadłospis zawiera wiele zarówno tradycyjnych jak również typowo europejskich dań.
 
-                var restaurant = new RestaurantOnCreateDto()
-                {
-                    Name = exampleRestaurantName[i],
-                    Description = $@"Restauracja {exampleRestaurantName[i]} jest od lat cenionym miejscem kameralnych spotkań, znanym ze znakomitej kuchni. Zapraszamy na  wyśmienity lunch w przerwie w pracy, wykwintny obiad z rodziną lub romantyczny wieczór na tarasach przy lampce wina.  Jadłospis zawiera wiele zarówno tradycyjnych jak również typowo europejskich dań.
-
-                    Szef kuchni szczególnie poleca - kotlet a'la {exampleRestaurantName[i]}, maczankę firmową (schab w sosie myśliwskim z grzankami), kotlet Pani Walewska,  naleśniki a'la {exampleRestaurantName[i]}.Wielu 'zwolenników' ma firmowa szarlotka z bitą śmietaną,
+                    Szef kuchni szczególnie poleca - kotlet a'la {
+                                    exampleRestaurantName[i]
+                                }, maczankę firmową (schab w sosie myśliwskim z grzankami), kotlet Pani Walewska, naleśniki a'la {
+                                    exampleRestaurantName[i]
+                                }. Wielu 'zwolenników' ma firmowa szarlotka z bitą śmietaną,
                     a także banany w sosie rumowym chętnie spożywane jako dodatek do kawy.
 
 
                     Szczególnie warte polecenia są również wina serwowane w restauracji.
 
                     W restauracji hotelu Maria odbywają się huczne wesela, przyjęcia okolicznościowe, bankiety firmowe. sylwestry.",
-                    Cuisine = exampleCousine[random.Next(0, exampleCousine.Count-1)],
-                    Localization = $"Szczecin",
-                    Tables = tables
-                }; // RestaurantEntity
-
-                await _restaurantService.RegisterAsync(restaurant);
-
-                var restaurantDto = (await _restaurantService.GetAllAsync()).Last();                
-
-                for (var z = 1; z <= 10; z++)
-                {
-                    var firstNameEmployee = exampleFirstName[random.Next(exampleFirstName.Count)];
-                    var lastNameEmployee = exampleLastName[random.Next(exampleLastName.Count)];
-                    var loginEmployee = $"employee{i}{z}";
-                    var emailEmployee = $"{loginEmployee}@gmail.com";
-                    var phoneEmployee = $"{i}{z}02345678";
-
-                    EmployeeOnCreateDto employee = new EmployeeOnCreateDto()
-                    {
-                        Login = loginEmployee,
-                        FirstName = firstNameEmployee,
-                        Email = emailEmployee,
-                        Phone = phoneEmployee,
-                        LastName = lastNameEmployee,
-                        Password = "!QAZxsw2",
-                        Restaurant = restaurantDto
+                        Cuisine = exampleCousine[random.Next(0, exampleCousine.Count - 1)],
+                        Localization = $"Szczecin",
+                        Tables = restaurantTables
                     };
 
-                    await _employeeService.RegisterAsync(employee);
-                } //Employee
-
-                var firstNameOwner = exampleFirstName[random.Next(exampleFirstName.Count)]; //OwnerEntity
-                var lastNameOwner = exampleLastName[random.Next(exampleLastName.Count)];
-                var loginOwner = $"owner{i}";
-                var emailOwner = $"{loginOwner}@gmail.com";
-                var phoneOwner = $"{i}02345678";
-
-                var owner = new OwnerOnCreateDto()
+                    await _restaurantService.RegisterAsync(restaurant);
+                }
+                restaurants = (await _restaurantService.GetAllAsync()).ToList();
+                // Restaurant Photo               
+                for (var i = 0; i < 10; i++)
                 {
-                    Login = loginOwner,
-                    FirstName = firstNameOwner,
-                    Email = emailOwner,
-                    Phone = phoneOwner,
-                    LastName = lastNameOwner,
-                    Password = "!QAZxsw2",
-                    Restaurant = restaurantDto
-                };
+                    var restaurant = restaurants.ElementAt(i);
+                    var restaurantGuid = restaurant.Id;
+                    File.Copy($"wwwroot/images/datainitializer/examplephotos/0{random.Next(0, 9)}.jpg", $"wwwroot/images/restaurants/{restaurantGuid}.jpg");
+                    restaurant.RepresentativePhotoUrl =
+                        $"http://zakleptoapi.azurewebsites.net/images/restaurants/{restaurantGuid}.jpg";
 
-                await _ownerService.RegisterAsync(owner);
+                    var restaurantWithPhoto = _mapper.Map<RestaurantDto, RestaurantOnUpdateDto>(restaurant);
+                    await _restaurantService.UpdateAsync(restaurantWithPhoto, restaurant.Id);
 
-                
-                for (var z = 0; z < tables.Count/2; z++)
-                {
-                    var date = new DateTime(2018, random.Next(1, 12), random.Next(1, 27), random.Next(1, 24), random.Next(1, 60), 0);
-                    var reservation = new ReservationOnCreateDto()
-                    {
-                        Restaurant = restaurantDto,
-                        Table = _mapper.Map<Table, TableDto>(tables.ElementAt(z)),
-                        DateStart = date,
-                        DateEnd = date.AddHours(3),
-                        Customer =  (await _customerService.GetAllAsync()).ElementAt(z),
-
-                     };
-
-                    await _reservationService.RegisterReservation(reservation);
-                }              
-            } //RestaurantEntity & OwnerEntity & Employee
-
-            var restaurants = (await _restaurantService.GetAllAsync()).ToList();
-
-            for(int i = 0; i < exampleRestaurantName.Count; i++)
-            {
-                var restaurant = restaurants.ElementAt(i);
-                var restaurantGuid = restaurant.Id;
-                File.Copy($"wwwroot/images/datainitializer/examplephotos/0{random.Next(0, 9)}.jpg", $"wwwroot/images/restaurants/{restaurantGuid}.jpg");
-                restaurant.RepresentativePhotoUrl =
-                    $"http://zakleptoapi.azurewebsites.net/images/restaurants/{restaurantGuid}.jpg";
-
-                var restaurantWithPhoto = _mapper.Map<RestaurantDto, RestaurantOnUpdateDto>(restaurant);
-                await _restaurantService.UpdateAsync(restaurantWithPhoto, restaurant.Id);
-
+                }
             }
 
+            // Employees
+            var employees = await _employeeService.GetAllAsync();
+            if(!employees.Any())
+            {
+                restaurants = (await _restaurantService.GetAllAsync()).ToList();
+                for (var i = 0; i < 10; i++)
+                {
+                    for (var j = 1; j <= 10; j++)
+                    {
+                        var employee = new EmployeeOnCreateDto()
+                        {
+                            Login = $"employee{i}{j}",
+                            FirstName = exampleFirstName[random.Next(exampleFirstName.Count)],
+                            LastName = exampleLastName[random.Next(exampleLastName.Count)],
+                            Email = $"employee{i}{j}@gmail.com",
+                            Phone = $"{i}{j}2345678",
+                            Password = "qwerty1",
+                            Restaurant = restaurants.ElementAt(i)
+                        };
+
+                        await _employeeService.RegisterAsync(employee);
+                    }
+                }
+            }
+
+            // Owners
+            var owners = await _ownerService.GetAllAsync();
+            if(!owners.Any())
+            {     
+                for (var i = 1; i <= 10; i++)
+                {
+                    var owner = new OwnerOnCreateDto()
+                    {
+                        Login = $"owner{i}",
+                        FirstName = exampleFirstName[random.Next(exampleFirstName.Count)],
+                        LastName = exampleLastName[random.Next(exampleLastName.Count)],
+                        Email = $"owner{i}@gmail.com",
+                        Phone = $"{i}32345678",
+                        Password = "qwerty1",
+                        Restaurant = restaurants.ElementAt(i-1)
+                    };
+
+                    await _ownerService.RegisterAsync(owner);
+                }
+            }
+
+            // Reservations
+            var reservations = await _reservationService.GetAllAsync();
+            if(!reservations.Any())
+            { 
+                var registeredCustomers = (await _customerService.GetAllAsync()).ToList();
+        
+                for (var i = 0; i < 10; i++)
+                {
+                    for (var j = 0; j < random.Next(5, 10); j++)
+                    {
+                        var restaurantId = random.Next(0, 9);
+                        var date = new DateTime(2018, random.Next(1, 12), random.Next(1, 27), random.Next(1, 24), random.Next(1, 60), 0);
+                        var reservation = new ReservationOnCreateDto()
+                        {
+                            Restaurant = restaurants.ElementAt(restaurantId),
+                            TableId = restaurants.ElementAt(restaurantId).Tables.ElementAt(random.Next(0,9)).Id,
+                            DateStart = date,
+                            DateEnd = date.AddHours(1),
+                            Customer = registeredCustomers.ElementAt(random.Next(0, 99)),
+                        };
+
+                        await _reservationService.RegisterReservation(reservation);
+                    }
+                }
+            }
+                      
+            // Reservation Confirm
             foreach (var reservationDto in await _reservationService.GetAllAsync())
             {
                 if (random.Next(1, 10) > 3)
